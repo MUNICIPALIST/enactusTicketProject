abb50afb4cb365b3f1965a0b1a7aab8ef803f018
valtzman <xmodgamecenterhon@gmail.com>
2025-04-04 14:48:27 +0500

    fix

--- Admin_Pannel/Front/vite.config.ts
+++ Admin_Pannel/Front/vite.config.ts
@@ -9,6 +9,24 @@ export default defineConfig({
     port: 5173,
     watch: {
       usePolling: true
-    }
-  }
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'vencera.tech',
+      '.vencera.tech' // Разрешает все поддомены
+    ]
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  base: '/'
 })
